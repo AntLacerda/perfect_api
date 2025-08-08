14307263069549c9bda7fc6c0fd9038a7b88f4e8
AntLacerda <lacerdaneto99@hotmail.com>
2025-08-08 16:09:50 -0300

    Feat: Desenvolvendo service de criação de usuário regular

--- src/services/User/index.ts
+++ src/services/User/index.ts
@@ -1,9 +1,9 @@
 import { createHashPassword, compareHashWithPassword } from "@src/utils/bcrypt";
 import { AppError } from "@src/errors/AppError";
-import { RegularUserDTO } from "@src/dtos/User";
+import { RegularUserDTO, AdminUserDTO } from "@src/dtos/User";
 import { prisma } from "@src/database";
 
-const createAdminUser = async (userData: RegularUserDTO) => {
+const createAdminUser = async (userData: AdminUserDTO) => {
     const { name, email, password } = userData;
     const emailNormalized = email.toLowerCase().trim();
 
@@ -56,4 +56,59 @@ const createAdminUser = async (userData: RegularUserDTO) => {
             };
     });
 }
+
+const createRegularUser = async (userData: RegularUserDTO) => {
+    const { name, email, password } = userData;
+    const emailNormalized = email.toLowerCase().trim();
+
+    return await prisma.$transaction(async (tx) => {
+        const existingUser = await tx.user.findUnique({
+                where: {
+                    email: emailNormalized
+                }
+            });
+
+            if (existingUser) {
+                throw new AppError("User already exists", 409, "USER_ALREADY_EXISTS");
+            }
+
+            const userPermission = await tx.permission.findFirst({
+                where: {
+                    role: "REGULAR_USER",
+                }
+            });
+
+            if (!userPermission) {
+                throw new AppError("User permission not found", 404, "PERMISSION_NOT_FOUND");
+            }
+
+            const userResponse = await tx.user.create({
+                data: {
+                    name,
+                    email: emailNormalized,
+                    password: await createHashPassword(password),
+                    Permission: {
+                        connect: {
+                            id: userPermission.id
+                        }
+                    }
+                },
+                select: {
+                    name: true,
+                    email: true,
+                    Permission: {
+                        select: {
+                            role: true,
+                        }
+                    }
+                }
+            });
+
+            return {
+                message: "Regular user created successfully",
+                data: userResponse
+            };
+    });
+}
+
     
