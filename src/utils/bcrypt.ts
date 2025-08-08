51093ec76bff97f2e6f196713466316e93d40e58
AntLacerda <lacerdaneto99@hotmail.com>
2025-08-08 15:36:16 -0300

    Feat: Desenvolvendo melhoria para função de criptografia

--- src/utils/bcrypt.ts
+++ src/utils/bcrypt.ts
@@ -1,15 +1,21 @@
 import bcrypt from "bcrypt";
+import { AppError } from "@src/errors/AppError";
 
-const SALT_ROUNDS = 10;
+const SALT_ROUNDS = Number(process.env.SALT_ROUNDS) || 10;
 
 export const createHashPassword = async (password: string): Promise<string> => {
-    const hashPassword = await bcrypt.hash(password, SALT_ROUNDS);
-
-    return hashPassword;
+    try {
+        return await bcrypt.hash(password, SALT_ROUNDS);
+    } catch (error) {
+        throw new AppError("Error creating hash password", 500, "HASH_CREATION_ERROR");
+    }
 }
 
 export const compareHashWithPassword = async (password: string, hashPassword: string): Promise<boolean> => {
-    const isValidPassword = await bcrypt.compare(password, hashPassword);
-
-    return isValidPassword;
+    try {
+        return await bcrypt.compare(password, hashPassword);
+    } catch (error) {
+        throw new AppError("Error comparing hash with password", 500, "HASH_COMPARISON_ERROR");
+        
+    }
 }
